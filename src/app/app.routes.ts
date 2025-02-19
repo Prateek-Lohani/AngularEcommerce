bc1f8b2306fd1b0a922ea3106c05ed07db9ff3f8
Prateek Lohani <prateeklohani105@gmail.com>
2025-02-19 14:54:28 +0530

    SIgnUp Form

---
+++ src/app/app.routes.ts
@@ -0,0 +1,30 @@
+import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { SellerAuthComponent } from './seller-auth/seller-auth.component';
+import { NotFoundComponent } from './not-found/not-found.component';
+import { LoginComponent } from './login/login.component';
+import { CartComponent } from './cart/cart.component';
+
+export const routes: Routes = [
+    {
+        path:'',
+        component: HomeComponent
+    },
+    {
+        path:'seller',
+        component: SellerAuthComponent
+    },
+    {
+        path:'login',
+        component:LoginComponent
+
+    },{
+        path:'cart',
+        component:CartComponent
+    },
+    {
+        path:'**',
+        pathMatch:'full',
+        component:NotFoundComponent
+    }
+];
---
+++ src/app/seller-auth/seller-auth.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-seller-auth',
+  standalone:true,
+  imports: [FormsModule],
+  templateUrl: './seller-auth.component.html',
+  styleUrl: './seller-auth.component.css'
+})
+export class SellerAuthComponent {
+
+  signUp(data:any):void{
+    console.log(data)
+  }
+
+}
